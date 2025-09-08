0112e5f2c34ff639d5d10666c938337ea435e819
shrupik-sadovnic <shrupik-sadovnic@mail.ru>
2025-09-08 12:17:28 +0600

    Add routes for auth

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-// const authRoutes = require("./auth_routes");
 // const usersRoutes = require("./user_routes");
+const authRoutes = require("./auth_routes");
 const tasksRoutes = require("./task_routes");
 
-// router.use("/auth", authRoutes);
 // router.use("/users", usersRoutes);
 router.use("/tasks", tasksRoutes);
+router.use("/auth", authRoutes);
 
 module.exports = router;
