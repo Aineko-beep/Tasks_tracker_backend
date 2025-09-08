0e8b2885d1145d22c06048220cfd82a940d2b22c
Ksenia1-n <broksusa455@gmail.com>
2025-09-08 09:06:49 +0300

    Add routes for user

---
+++ src/routes/user.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+
+ // GET: /api/usre
+router.get("/", (req, res) => {
+    res.json([{ id: 1, title: "List of users", status: "new" }]);
+});
+
+//GET: /api/user/id
+router.get("/:id", (req, res) => {
+    res.json({ id: req.params.id, title: "List of users", status: "done" });
+});
+
+//POST: /api/user
+router.post("/", (req, res) => {
+    res.json({ message: "A user has been created" });
+});
+
+//PATCH: /api/user/id
+router.put("/:id", (req, res) => {
+    res.json({ message: "updated user" });
+});
+
+//DELETE: /api/user/id
+router.delete("/:id", (req, res) => {
+    res.json({ message: "the user has been deleted" });
+});
+
+module.exports = router;
